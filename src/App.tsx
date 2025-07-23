c1651f7d929cc04935a330bf934a56046668c0ff
joshthomas <jthoms1@gmail.com>
2025-07-23 14:57:53 -0500

    add setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { Button, defaultTheme, Provider } from "@adobe/react-spectrum";
+import { defaultTheme, Provider } from "@adobe/react-spectrum";
 
 function App() {
-  return (
-    <Provider theme={defaultTheme}>
-      <Button variant="accent" onPress={() => alert("Hey there!")}>
-        Hello React Spectrum!
-      </Button>
-    </Provider>
-  );
+  return <Provider theme={defaultTheme}>Hello</Provider>;
 }
 
 export default App;
